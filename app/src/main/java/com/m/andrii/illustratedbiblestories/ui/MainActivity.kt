ecac07e7b95658e6e5abd217343173003f01bde8
Andrew Miroshnichenko <mobilemiron@gmail.com>
2023-05-21 02:12:38 +0300

    feature: added ViewPager2

--- app/src/main/java/com/m/andrii/illustratedbiblestories/ui/MainActivity.kt
+++ app/src/main/java/com/m/andrii/illustratedbiblestories/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.m.andrii.illustratedbiblestories
+package com.m.andrii.illustratedbiblestories.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.m.andrii.illustratedbiblestories.R
 
 class MainActivity : AppCompatActivity() {
 
