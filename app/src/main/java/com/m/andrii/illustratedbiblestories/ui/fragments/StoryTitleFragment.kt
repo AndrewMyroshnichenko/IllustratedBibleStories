40daf5c35b51a4fff42cf793a22e97bc0c7b4cff
Andrew Miroshnichenko <mobilemiron@gmail.com>
2023-05-21 23:54:10 +0300

    feature: added fragment_story_title.xml

---
+++ app/src/main/java/com/m/andrii/illustratedbiblestories/ui/fragments/StoryTitleFragment.kt
@@ -0,0 +1,25 @@
+package com.m.andrii.illustratedbiblestories.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.m.andrii.illustratedbiblestories.R
+
+class StoryTitleFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_story_title, container, false)
+    }
+
+}
